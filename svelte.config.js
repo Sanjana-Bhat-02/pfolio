ccf76c3286067543ab95ae14a235cc4c78e27f18
sanju2728 <sanjanabhat2002@gmail.com>
2023-05-09 21:29:23 +0530

    what is happening

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		
-		adapter: vercel()
+		adapter: adapter({
+			runtime: 'nodejs18.x',
+		  }),
 	}
 };
 
