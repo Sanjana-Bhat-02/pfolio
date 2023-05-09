e236a7fac1e782ebc85cef42133bae4becb6743a
sanju2728 <sanjanabhat2002@gmail.com>
2023-05-09 20:11:50 +0530

    Updates

---
+++ _app/immutable/chunks/0.2aa0e37e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_layout.svelte.f90eebf9.js";export{t as component};
---
+++ _app/immutable/chunks/1.99e7fdbf.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.a89d25c1.js";export{t as component};
