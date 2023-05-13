56fb6335f19f715300c0e3a49ec0d8ff40b748d9
sanju2728 <sanjanabhat2002@gmail.com>
2023-05-13 23:56:10 +0530

    feat: added text effect

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -1,7 +1,7 @@
 <script>
     import { onMount } from "svelte";
     import Header from "../components/Header.svelte";
-  import Footer from "../components/Footer.svelte";
+    import Footer from "../components/Footer.svelte";
     let particlesConfig = {
         particles: {
             number: {
@@ -126,121 +126,262 @@
 </script>
 
 <div class="wrapper">
-  <div id="particles-js" class="bg-slate-900"></div>
-  <Header/>
-  <div class="con">
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <h1>fsdhfkdsfhd</h1>
-    <Footer/>
-  </div>
-  
+    <div id="particles-js" class="bg-zinc-900" />
+    <Header />
+    <div class="con">
+        <div class="sign">
+            <span class="fast-flicker">Hey&nbsp;</span>There!&nbsp;<span
+                class="flicker">I'm&nbsp;</span
+            >Sanjana
+        </div>
+    </div>
+
+    <div class="subtitle">
+        <p>Aspiring</p>
+        <p>Frontend</p>
+        <p>Developer</p>
+    </div>
+    <Footer />
 </div>
 
-
-
 <style>
-  .wrapper {
-    position: relative;
-    height: 100vh;
-}
+    @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+    .subtitle {
+        margin-top: 20px;
+        display: flex;
+        color: white;
+    }
+    .wrapper {
+        position: relative;
+        height: 100vh;
+    }
 
-  #particles-js {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: -1;
-    background-attachment: fixed;
-  }
+    #particles-js {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 0;
+        background-attachment: fixed;
+    }
 
-  .con {
-    font-size: 100%;
-    position: relative;
-    top: 90%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    flex: 1;
-    text-align: center;
-    
-  }
+    .con {
+        font-size: 100%;
+        position: relative;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+        margin-top: 40px;
+    }
 
-  h1 {
-		color: #ffffff;
-		text-transform: uppercase;
-		font-size: 35px;
-		
-		
-	}
+    .sign {
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 50%;
+        height: 50%;
+        background-image: radial-gradient(
+            ellipse 50% 35% at 50% 50%,
+            #6b1839,
+            transparent
+        );
+        transform: translate(-50%, -50%);
+        letter-spacing: 2;
+        left: 50%;
+        top: 50%;
+        font-family: "Poppins";
+        text-transform: uppercase;
+        font-size: 3em;
+        color: #ffe6ff;
+        text-shadow: 0 0 0.6rem #ffe6ff, 0 0 1.5rem #ff65bd,
+            -0.2rem 0.1rem 1rem #ff65bd, 0.2rem 0.1rem 1rem #ff65bd,
+            0 -0.5rem 2rem #ff2483, 0 0.5rem 3rem #ff2483;
+        animation: shine 2s forwards, flicker 3s infinite;
+    }
 
-	@keyframes fadeIn {
-		from {
-			opacity: 0;
-		}
-		to {
-			opacity: 1;
-		}
-	}
+    p {
+        color: white;
+        font-size: 2em;
+        position: absolute;
+        top: 100%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+    @keyframes blink {
+        0%,
+        22%,
+        36%,
+        75% {
+            color: #ffe6ff;
+            text-shadow: 0 0 0.6rem #ffe6ff, 0 0 1.5rem #ff65bd,
+                -0.2rem 0.1rem 1rem #ff65bd, 0.2rem 0.1rem 1rem #ff65bd,
+                0 -0.5rem 2rem #ff2483, 0 0.5rem 3rem #ff2483;
+        }
+        28%,
+        33% {
+            color: #ff65bd;
+            text-shadow: none;
+        }
+        82%,
+        97% {
+            color: #ff2483;
+            text-shadow: none;
+        }
+    }
+
+    .flicker {
+        animation: shine 2s forwards, blink 3s 2s infinite;
+    }
+
+    .fast-flicker {
+        animation: shine 2s forwards, blink 10s 1s infinite;
+    }
+
+    @keyframes shine {
+        0% {
+            color: #6b1839;
+            text-shadow: none;
+        }
+        100% {
+            color: #ffe6ff;
+            text-shadow: 0 0 0.6rem #ffe6ff, 0 0 1.5rem #ff65bd,
+                -0.2rem 0.1rem 1rem #ff65bd, 0.2rem 0.1rem 1rem #ff65bd,
+                0 -0.5rem 2rem #ff2483, 0 0.5rem 3rem #ff2483;
+        }
+    }
+
+    @keyframes flicker {
+        from {
+            opacity: 1;
+        }
+
+        4% {
+            opacity: 0.9;
+        }
+
+        6% {
+            opacity: 0.85;
+        }
+
+        8% {
+            opacity: 0.95;
+        }
+
+        10% {
+            opacity: 0.9;
+        }
+
+        11% {
+            opacity: 0.922;
+        }
+
+        12% {
+            opacity: 0.9;
+        }
+
+        14% {
+            opacity: 0.95;
+        }
+
+        16% {
+            opacity: 0.98;
+        }
+
+        17% {
+            opacity: 0.9;
+        }
+
+        19% {
+            opacity: 0.93;
+        }
+
+        20% {
+            opacity: 0.99;
+        }
+
+        24% {
+            opacity: 1;
+        }
+
+        26% {
+            opacity: 0.94;
+        }
+
+        28% {
+            opacity: 0.98;
+        }
+
+        37% {
+            opacity: 0.93;
+        }
+
+        38% {
+            opacity: 0.5;
+        }
+
+        39% {
+            opacity: 0.96;
+        }
+
+        42% {
+            opacity: 1;
+        }
+
+        44% {
+            opacity: 0.97;
+        }
+
+        46% {
+            opacity: 0.94;
+        }
+
+        56% {
+            opacity: 0.9;
+        }
+
+        58% {
+            opacity: 0.9;
+        }
+
+        60% {
+            opacity: 0.99;
+        }
+
+        68% {
+            opacity: 1;
+        }
+
+        70% {
+            opacity: 0.9;
+        }
+
+        72% {
+            opacity: 0.95;
+        }
+
+        93% {
+            opacity: 0.93;
+        }
+
+        95% {
+            opacity: 0.95;
+        }
+
+        97% {
+            opacity: 0.93;
+        }
+
+        to {
+            opacity: 1;
+        }
+    }
+
+    @media (max-width: 768px) {
+        .sign {
+            flex-direction: column;
+        }
+    }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,25 +5,3 @@
 <Main />
 
 
-<style>
-	h1 {
-		color: #ffffff;
-		text-transform: uppercase;
-		font-size: 300%;
-		font-weight: 100;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		z-index: 8;
-	}
-
-	@keyframes fadeIn {
-		from {
-			opacity: 0;
-		}
-		to {
-			opacity: 1;
-		}
-	}
-</style>
