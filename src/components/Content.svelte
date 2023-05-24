455d44d02dfd08f6eb87fba0e48932075e9acb32
sanju2728 <sanjanabhat2002@gmail.com>
2023-05-24 22:44:22 +0530

    starting afresh

--- src/components/Content.svelte
+++ src/components/Content.svelte
@@ -3,45 +3,41 @@
     import Footer from "../components/Footer.svelte";
 </script>
 
-<div class="flex flex-col relative h-screen">
+<div class="flex flex-col">
    
-    <div class="container">
-        <div class="con">
-            <div class="sign">
-                <span class="fast-flicker">Hey&nbsp;</span>There!&nbsp;<span
-                    class="flicker">I'm&nbsp;</span
-                >Sanjana
-            </div>
-        </div>
+  
+      
+      
 
-        <div class="subtitle">
-            <p class="word">Aspiring&nbsp;</p>
-            <p class="word">Frontend&nbsp;</p>
-            <p class="word">Developer&nbsp;</p>
-        </div>
-        
-
-        <section class="text-gray-600 body-font">
-            <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-              <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
-                  <br class="hidden lg:inline-block">readymade gluten
-                </h1>
-                <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
-                <div class="flex justify-center">
-                  <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-                  <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
-                </div>
-              </div>
-              <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
-              </div>
-            </div>
-          </section>
-        
-          
-        
+  <section class="text-gray-600 body-font lg:ml-20">
+    <div class="container mx-auto flex flex-col-reverse px-5 py-24 md:flex-row flex-col items-center">
+      
+      <div class="lg:max-w-md lg:w-2/5 md:w-1/2 w-5/6 lg:mr-20 lg:mt-20 order-last mb:order-first">
+        <img class="object-cover object-center rounded-full border-white" alt="hero" src="/me.jpeg">
+      </div>
+      
+      
+      <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 mb: mt-10 items-center justify-center text-center">
+        <h1 class="title-font md:text-6xl sm:text-6xl text-3xl mb-4 font-medium text-white ">Hey there!
+          <br class="hidden lg:inline-block mt-10">I'm Sanjana Bhat
+        </h1>
+        <p class="mb-8 leading-relaxed text-2xl">Computer Science Student. Aspiring Frontend Developer.<br>Loves Cats.</p>
+        <a href="https://github.com/sanju2728" target="_blank">
+          <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Go to GitHub</button>
+        </a>
+      </div>
+      
     </div>
+  </section>
+
+
+  
+  
+    
+       
+      
+ 
 </div>
+
 
 
